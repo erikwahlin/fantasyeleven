1cae37d1f13cf92313458b6d58f82326c422a221
butalsoverycool <kiiim@kiiim.se>
2020-03-30 17:56:56 +0200

    WIP filter/order

--- src/components/PlayerSearch/index.js
+++ src/components/PlayerSearch/index.js
@@ -2,42 +2,144 @@ import React, { Component } from 'react';
 //import logo from './logo.svg';
 //import '../App.css';
 
+const config = {
+	positions: ['Goalkeeper', 'Defender', 'Midfielder', 'Forward'],
+	sortOptions: {
+		name: 'namn',
+		team: 'lag',
+		price: 'pris',
+		position: 'position'
+	}
+};
+
 const INITIAL_STATE = {
+	position: 'default',
+	team: 'default',
+	maxPrice: '',
+	searchTerm: '',
+
+	sortBy: 'default',
+	sortOrder: '<',
+
 	price: {
 		lowHigh: false,
 		highLow: false
-	},
-	searchTerm: '',
-	team: ''
+	}
 };
+
+//orderby
+// namn alfabetiskt, lag alfabetiskt, pris, position
+// asc or desc
 
 class PlayerSearch extends Component {
 	constructor(props) {
 		super(props);
 
 		this.state = { ...INITIAL_STATE };
+
 		this.handleTextFilterChange = this.handleTextFilterChange.bind(this);
 		this.handleSortAsc = this.handleSortAsc.bind(this);
 		this.handleSortDesc = this.handleSortDesc.bind(this);
 		this.handleTeamOptionChange = this.handleTeamOptionChange.bind(this);
-		this.filterByName = this.filterByName.bind(this);
+
+		this.updateState = this.updateState.bind(this);
+		this.resetSettings = this.resetSettings.bind(this);
+
+		this.maxPriceHandler = this.maxPriceHandler.bind(this);
+
+		this.filterByPosition = this.filterByPosition.bind(this);
 		this.filterByTeam = this.filterByTeam.bind(this);
+		this.filterByMaxPrice = this.filterByMaxPrice.bind(this);
+		this.filterByName = this.filterByName.bind(this);
 	}
 
-	filterByName = (playersList, searchTerm) => {
-		searchTerm = searchTerm.trim().toLowerCase();
-		return playersList.filter(elem => {
-			return elem.name.toLowerCase().includes(searchTerm);
+	// update settings in state
+	updateState = (key, val) => {
+		this.setState({
+			[key]: val
 		});
 	};
 
-	filterByTeam = (playerList, team) => {
+	// reset filter & order
+	resetSettings = () => {
+		this.setState({
+			...INITIAL_STATE
+		});
+	};
+
+	// parse price-input before state update
+	maxPriceHandler = maxPrice => {
+		this.setState({
+			maxPrice: parseFloat(maxPrice)
+		});
+	};
+
+	// filter-funcs
+	filterByPosition = playerList => {
+		const pos = this.state.position;
+		if (!pos || pos === 'default') return playerList;
+
+		return playerList.filter(player => player.position === pos);
+	};
+
+	filterByTeam = playerList => {
+		const team = this.state.team;
 		if (!team || team === 'default') return playerList;
 
 		return playerList.filter(player => player.team === team);
 	};
 
-	handleSortAsc() {
+	filterByMaxPrice = playerList => {
+		const maxPrice = this.state.maxPrice;
+		if (isNaN(maxPrice) || !maxPrice || maxPrice === '' || maxPrice <= 0)
+			return playerList;
+
+		return playerList.filter(player => parseFloat(player.price) <= maxPrice);
+	};
+
+	filterByName = playersList => {
+		const searchTerm = this.state.searchTerm.trim().toLowerCase();
+		return playersList.filter(player => {
+			return player.name.toLowerCase().includes(searchTerm);
+		});
+	};
+
+	// sort by (WIP, needs cleaner structure)
+	applySortBy = playerList => {
+		const { sortBy, sortOrder } = this.state;
+
+		const sortIt = (a, b) => {
+			if (sortBy === 'position') {
+				const sortValA = config.positions.indexOf(a[sortBy]);
+				const sortValB = config.positions.indexOf(b[sortBy]);
+
+				if (sortValA < sortValB) {
+					return sortOrder === '<' ? -1 : 1;
+				} else {
+					return sortOrder === '<' ? 1 : -1;
+				}
+			} else if (sortBy === 'price') {
+				const sortValA = parseFloat(a[sortBy]);
+				const sortValB = parseFloat(b[sortBy]);
+
+				if (sortValA < sortValB) {
+					return sortOrder === '<' ? -1 : 1;
+				} else {
+					return sortOrder === '<' ? 1 : -1;
+				}
+			}
+
+			if (a[sortBy] < b[sortBy] && sortBy !== 'price') {
+				return sortOrder === '<' ? -1 : 1;
+			} else {
+				return sortOrder === '<' ? 1 : -1;
+			}
+		};
+
+		return sortBy === 'default' ? playerList : playerList.sort(sortIt);
+	};
+
+	/* handleSortAsc() {
 		this.setState(prevState => ({
 			price: {
 				// object that we want to update
@@ -46,6 +148,7 @@ class PlayerSearch extends Component {
 			}
 		}));
 	}
+
 	handleSortDesc() {
 		this.setState(prevState => ({
 			price: {
@@ -54,7 +157,7 @@ class PlayerSearch extends Component {
 				lowHigh: !this.state.lowHigh // update the value of specific key
 			}
 		}));
-	}
+	} */
 
 	handleTextFilterChange(event) {
 		this.setState({ searchTerm: event.target.value });
@@ -77,47 +180,42 @@ class PlayerSearch extends Component {
 
 		console.log('all teams', teams); // ['arsenal', 'aston villa'] -- ger alltså tillbaka en array med alla team i PL.
 
-		//now - filter players array depending on Team? eg - user choose aston villa
-		//sort by price. ascending and descending.
-
-		//eventuellt göra om till en function med lite instruktioner som parameter 2.
-		const sortPriceAsc = playerList => {
-			return playerList.sort((a, b) => a.price - b.price);
-		};
-
-		const sortPriceDesc = playerList => {
-			return playerList.sort((a, b) => b.price - a.price);
-		};
-
-		//sorterar från högt till lågt.
-		const groupByTeam = (playerList, team = 'aston villa') => {
-			return playerList.filter(player => {
-				return team.indexOf(player.team.toLowerCase()) >= 0;
-			});
-		};
-
-		let team = 'aston villa';
-		console.log(groupByTeam(players, team)); // team är alltså det laget användaren väljer.
-		console.log(sortPriceDesc(players)); //bygger ny array av objekt och ger tillbaka astonvilla-spelare.
-		console.log(sortPriceAsc(players));
-
-		// Apply search filters
-		const filtered = this.filterByTeam(
-			this.filterByName(players, this.state.searchTerm),
-			this.state.team
+		// Apply filters
+		const filtered = this.filterByPosition(
+			this.filterByTeam(this.filterByMaxPrice(this.filterByName(players)))
 		);
+
+		// Apply order-config
+		const sorted = this.applySortBy(filtered);
+
+		const { position, team, maxPrice, searchTerm } = this.state;
 
 		return (
 			<div className='App'>
-				<button onClick={e => this.handleSortAsc(e)}>lågt till högt</button>
-				<button onClick={e => this.handleSortDesc(e)}>
-					högt till lågt
-				</button>
+				{/* FILTER */}
 
+				{/* Position filter */}
 				<select
-					onChange={event => this.handleTeamOptionChange(event)}
+					onChange={e => this.updateState('position', e.target.value)}
+					value={position}
+				>
+					<option value='default'>- Alla positioner -</option>
+					{config.positions.map(pos => {
+						return (
+							<option key={pos} value={pos}>
+								{pos}
+							</option>
+						);
+					})}
+				</select>
+
+				<br />
+
+				{/* Team filter */}
+				<select
+					onChange={e => this.updateState('team', e.target.value)}
 					id='teams'
-					defaultValue='default'
+					value={team}
 				>
 					<option value='default'>- Alla lag -</option>
 					{teams.map(team => {
@@ -129,21 +227,64 @@ class PlayerSearch extends Component {
 					})}
 				</select>
 
+				<br />
+
+				{/* Max Price filter */}
 				<input
-					name='name'
-					onChange={this.handleTextFilterChange}
-					placeholder='Spelarnamn ...'
+					type='number'
+					step='0.1'
+					onChange={e => this.maxPriceHandler(e.target.value)}
+					placeholder='Maxpris (milj.)'
+					value={maxPrice}
 				></input>
 
+				<br />
+
+				{/* Player name filter */}
+				<input
+					type='text'
+					name='name'
+					onChange={this.handleTextFilterChange}
+					placeholder='Spelarnamn'
+				></input>
+
+				{/* SORT */}
+
+				{/* Sort By */}
+				<select
+					onChange={e => this.updateState('sortBy', e.target.value)}
+					defaultValue='default'
+				>
+					<option value='default'>- Sortera efter -</option>
+					{Object.entries(config.sortOptions).map(([key, val]) => {
+						return (
+							<option key={key} value={key}>
+								{val}
+							</option>
+						);
+					})}
+				</select>
+
+				{/* Sort Order */}
+				<select
+					onChange={e => this.updateState('sortOrder', e.target.value)}
+					defaultValue='<'
+				>
+					<option value='<'>Stigande</option>
+					<option value='>'>Fallande</option>
+				</select>
+
+				<button onClick={this.resetSettings}>Återställ filter</button>
+
+				<br />
+
+				{/* RESULT */}
 				<ul>
-					{filtered.map((elem, i) => {
+					{sorted.map((elem, i) => {
 						return (
 							<li key={i}>
-								<strong>{elem.name}</strong>
-								{' PRIS: '}
-								{elem.price}
-								{'Milj'}
-								{' lag: '} {elem.team}
+								<strong>{elem.name}</strong> Lag: {elem.team} |
+								Position: {elem.position} | Pris: {elem.price}milj.
 							</li>
 						);
 					})}
