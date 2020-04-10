e4396977f197a9cf5bcf97c3bd3097bc7f35956a
butalsoverycool <kiiim@kiiim.se>
2020-04-10 17:07:18 +0200

    mongo test temp

--- src/index.js
+++ src/index.js
@@ -9,6 +9,42 @@ import * as serviceWorker from './serviceWorker';
 import App from './components/App';
 import Firebase, { FirebaseContext } from './components/Firebase';
 
+import { MongoClient } from 'mongodb';
+
+import { format } from 'util';
+
+/*
+ * MONGO TEST TEMP
+ */
+//import assert from 'assert';
+var user = encodeURIComponent(process.env.REACT_APP_MONGO_USER);
+var password = encodeURIComponent(process.env.REACT_APP_MONGO_PWS);
+var host = encodeURIComponent(process.env.REACT_APP_MONGO_HOST);
+var port = encodeURIComponent(process.env.REACT_APP_MONGO_PORT);
+var authMechanism = 'DEFAULT';
+
+// Connection URL
+var uri = format(
+	'mongodb+srv://%s:%s@%s:%s/test?retryWrites=true&w=majority',
+	user,
+	password,
+	host,
+	port,
+	authMechanism
+);
+
+//const MongoClient = require('mongodb').MongoClient;
+
+const client = new MongoClient(uri, { useNewUrlParser: true });
+client.connect(err => {
+	if (err) {
+		console.log('mongo err', err);
+	}
+
+	// ??? const collection = client.db('test').collection('devices');
+	client.close();
+});
+
 ReactDOM.render(
 	<FirebaseContext.Provider value={new Firebase()}>
 		<App />
