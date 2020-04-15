f08d77e08aad27219411319b664148420c7f2f20
Carl Lindgren <carlindgren@outlook.com>
2020-04-15 21:57:39 +0200

    fixed translation for message

--- src/components/instructionsWrapper/instructionsWrapper.jsx
+++ src/components/instructionsWrapper/instructionsWrapper.jsx
@@ -8,26 +8,25 @@ const StyledDiv = styled.div`
     font-size: 1.3em;
     width: inherit;
     width: 90%;
-    
 `
 const StyledP = styled.p`
 padding: 10px;
 `
 const InstructionsWrapper = ({ buildStagePage, posOrClub, list, maxPrice }) => {
     //posOrClub = toSwe(posOrClub, 'positions', 'plural')
+    console.log(posOrClub !== 'Alla spelare' ? toSwe(posOrClub, 'positions') : posOrClub)
     return (
 
         <StyledDiv>
             <StyledP>
                 {buildStagePage === 0 && list.length < 11 ?
                     (
-                        `Du kan inte ha fler ${posOrClub !== 'Alla spelare' ? posOrClub : 'spelare'}  på planen`
+                        `Du kan inte ha fler ${posOrClub !== 'Alla spelare' ? toSwe(posOrClub, 'positions', 'plural').toLowerCase() : 'spelare'}  på planen`
                     ) : (
-                        buildStagePage === 1 && list.length < 15 ? (
-                            'Du kan bara ha 1 spelare från varje position till ett värde av max 30 kr.'
-                        )
-                            :
+                        buildStagePage === 1 && list.length < 15 ?
                             (
+                                'Du kan bara ha 1 spelare från varje position till ett värde av max 30 kr.'
+                            ) : (
                                 `Klicka på vidare för att ta dig till nästa steg.${buildStagePage === 1 ? ' Kolla så att du inte gått över budgeten.' : ''}`
                             )
 
