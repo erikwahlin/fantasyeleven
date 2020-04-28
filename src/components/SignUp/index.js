3f3f0fadb62322bfff0f002d61f4ae24b2614092
Carl Lindgren <carlindgren@outlook.com>
2020-04-28 14:57:40 +0200

    styled signupform

--- src/components/SignUp/index.js
+++ src/components/SignUp/index.js
@@ -4,52 +4,70 @@ import { withFirebase } from '../Firebase';
 import * as ROUTES from '../../constants/routes';
 import * as ROLES from '../../constants/roles';
 import styled from 'styled-components';
+import FormInput from '../FormInput/FormInput';
 
 const Form = styled.form`
-	margin: 0 auto;
-	width: 50%;
-	display: flex;
-	flex-direction: column;
+    display: flex;
+    flex-direction: column;
 `;
-const InputWrap = styled.div`
-	color: white;
-	display: flex;
-	flex-direction: column;
-	margin-top: 10px;
-`;
+
 const Button = styled.button`
-	width: 90px;
-	height: 40px;
-	align-self: flex-end;
-	color: white;
-	background-color: ;
+    width: 120px;
+    height: 60px;
+    /* align-self: flex-end; */
+    color: white;
+    background-color: ${props => (props.disabled === false ? '#001c3e' : 'lightgrey')};
+    border: 1px solid white;
+    &:hover {
+        cursor: ${props => (props.disabled === false ? 'pointer' : 'normal')};
+        background-color: ${props => (props.disabled === false ? 'white' : '')};
+        color: ${props => (props.disabled === false ? 'black' : '')};
+    }
+
+    @media all and (max-width: 480px) {
+        width: 100vw;
+    }
 `;
-const Input = styled.input`
-	background-color: rgba(2, 31, 61, 1);
-	color: white;
-	outline: none;
-	height: 1.9em;
-	font-size: 1em;
-	border: none;
-	border-bottom: 1px solid white;
+const StyledH1 = styled.h1`
+    text-align: center;
+    /* margin-top: 20px; */
+    font-size: 3em;
 `;
+
 const Wrapper = styled.div`
-	background-color: rgba(2, 31, 61, 1);
+    border-radius: 10px;
+    box-shadow: 0px 1px 30px -6px rgba(255, 255, 255, 0.91);
+    margin: 0 auto;
+    width: 40%;
+    background-color: #001c3e;
+    justify-content: center;
+    display: flex;
+    flex-direction: column;
+    @media all and (max-width: 480px) {
+        width: 100vw;
+        box-shadow: none;
+        justify-content: space-between;
+        height: 100vh;
+        /* bottom: 0; */
+    }
+    @media all and (min-width: 481px) and (max-width: 1200px) {
+        width: 80vw;
+    }
 `;
 const SignUpPage = () => (
-	<Wrapper>
-		<h1>Börja spela</h1>
-		<SignUpForm />
-	</Wrapper>
+    <Wrapper>
+        <StyledH1>Skapa ett konto</StyledH1>
+        <SignUpForm />
+    </Wrapper>
 );
 
 const INITIAL_STATE = {
-	username: '',
-	email: '',
-	passwordOne: '',
-	passwordTwo: '',
-	isAdmin: false,
-	error: null
+    username: '',
+    email: '',
+    passwordOne: '',
+    passwordTwo: '',
+    isAdmin: false,
+    error: null
 };
 
 const ERROR_CODE_ACCOUNT_EXISTS = 'auth/email-already-in-use';
@@ -63,98 +81,137 @@ const ERROR_MSG_ACCOUNT_EXISTS = `
 `;
 
 class SignUpFormBase extends Component {
-	constructor(props) {
-		super(props);
+    constructor(props) {
+        super(props);
 
-		this.state = { ...INITIAL_STATE };
-	}
+        this.state = { ...INITIAL_STATE };
+    }
 
-	onSubmit = event => {
-		const { username, email, passwordOne, isAdmin } = this.state;
-		const roles = [];
+    onSubmit = event => {
+        const { username, email, passwordOne, isAdmin } = this.state;
+        const roles = [];
 
-		if (isAdmin) {
-			roles.push(ROLES.ADMIN);
-		}
+        if (isAdmin) {
+            roles.push(ROLES.ADMIN);
+        }
 
-		this.props.firebase
-			.doCreateUserWithEmailAndPassword(email, passwordOne)
-			.then(authUser => {
-				// Create a user in your Firebase realtime database
-				return this.props.firebase.user(authUser.user.uid).set({
-					username,
-					email,
-					roles
-				});
-			})
-			//      .then(() => {
-			//        return this.props.firebase.doSendEmailVerification();
-			//      })
-			.then(() => {
-				this.setState({ ...INITIAL_STATE });
-				this.props.history.push(ROUTES.HOME);
-			})
-			.catch(error => {
-				if (error.code === ERROR_CODE_ACCOUNT_EXISTS) {
-					error.message = ERROR_MSG_ACCOUNT_EXISTS;
-				}
+        this.props.firebase
+            .doCreateUserWithEmailAndPassword(email, passwordOne)
+            .then(authUser => {
+                // Create a user in your Firebase realtime database
+                return this.props.firebase.user(authUser.user.uid).set({
+                    username,
+                    email,
+                    roles
+                });
+            })
+            //      .then(() => {
+            //        return this.props.firebase.doSendEmailVerification();
+            //      })
+            .then(() => {
+                this.setState({ ...INITIAL_STATE });
+                this.props.history.push(ROUTES.HOME);
+            })
+            .catch(error => {
+                if (error.code === ERROR_CODE_ACCOUNT_EXISTS) {
+                    error.message = ERROR_MSG_ACCOUNT_EXISTS;
+                }
 
-				this.setState({ error });
-			});
+                this.setState({ error });
+            });
 
-		event.preventDefault();
-	};
+        event.preventDefault();
+    };
 
-	onChange = event => {
-		this.setState({ [event.target.name]: event.target.value });
-	};
+    onChange = event => {
+        const { name, value } = event.target;
+        this.setState({ [name]: value });
+    };
 
-	onChangeCheckbox = event => {
-		this.setState({ [event.target.name]: event.target.checked });
-	};
+    onChangeCheckbox = event => {
+        const { name, checked } = event.target;
+        this.setState({ [name]: checked });
+    };
 
-	render() {
-		const { username, email, passwordOne, passwordTwo, isAdmin, error } = this.state;
+    render() {
+        const { username, email, passwordOne, passwordTwo, isAdmin, error } = this.state;
+        const pwValidation = (pw1, pw2) => {
+            if ((pw1.length === 0 && pw2.length === 0) || pw2.length === 0) {
+                return '';
+            }
+            if (pw2.length < 6) {
+                return 'ditt lösenord är kortare än 6 bokstäver';
+            }
+            if (pw1.length >= 6 && pw2.length >= 6 && pw1 !== pw2) {
+                return 'dina lösenord är inte identiska';
+            }
+        };
+        const isPWInvalid =
+            passwordTwo.length < 6 || passwordOne !== passwordTwo || passwordTwo.length < 6;
+        const isInvalid =
+            passwordOne !== passwordTwo || passwordOne === '' || email === '' || username === '';
 
-		const isInvalid =
-			passwordOne !== passwordTwo || passwordOne === '' || email === '' || username === '';
+        return (
+            <Form onSubmit={this.onSubmit}>
+                <FormInput
+                    name="username"
+                    value={username}
+                    onChange={this.onChange}
+                    type="text"
+                    label="Användarnamn"
+                    required
+                />
+                <FormInput
+                    name="email"
+                    value={email}
+                    onChange={this.onChange}
+                    type="text"
+                    label="Mailadress"
+                    required
+                />
+                <FormInput
+                    name="passwordOne"
+                    value={passwordOne}
+                    onChange={this.onChange}
+                    type="password"
+                    isInvalid={isPWInvalid}
+                    label="Lösenord"
+                    required
+                />
+                {error && <p>{error.message}</p>}
+                <FormInput
+                    name="passwordTwo"
+                    value={passwordTwo}
+                    onChange={this.onChange}
+                    type="password"
+                    isInvalid={isPWInvalid}
+                    label="Lösenord Igen"
+                    required
+                />
+                {<p>{pwValidation(passwordOne, passwordTwo)}</p>}
+                {/*                 <label>
+                    Admin:
+                    <input
+                        name="isAdmin"
+                        type="checkbox"
+                        checked={isAdmin}
+                        onChange={this.onChangeCheckbox}
+                    />
+                </label> */}
+                <Button disabled={isInvalid} type="submit">
+                    Registrera
+                </Button>
 
-		return (
-			<Form onSubmit={this.onSubmit}>
-				<InputWrap>
-					Namn
-					<Input name="username" value={username} onChange={this.onChange} type="text" />
-				</InputWrap>
-				<InputWrap>
-					Email
-					<Input name="email" value={email} onChange={this.onChange} type="text" />
-				</InputWrap>
-				<InputWrap>
-					Lösenord
-					<Input name="passwordOne" value={passwordOne} onChange={this.onChange} type="password" />
-				</InputWrap>
-				<InputWrap>
-					Lösenord igen
-					<Input name="passwordTwo" value={passwordTwo} onChange={this.onChange} type="password" />
-				</InputWrap>
-				<label>
-					Admin:
-					<input name="isAdmin" type="checkbox" checked={isAdmin} onChange={this.onChangeCheckbox} />
-				</label>
-				<Button disabled={isInvalid} type="submit">
-					Registrera
-				</Button>
-
-				{error && <p>{error.message}</p>}
-			</Form>
-		);
-	}
+                {/* error && <p>{error.message}</p> */}
+            </Form>
+        );
+    }
 }
 
 const SignUpLink = () => (
-	<p>
-		Har du inget konto? <Link to={ROUTES.SIGN_UP}>Registrera dig</Link>
-	</p>
+    <p>
+        Har du inget konto? <Link to={ROUTES.SIGN_UP}>Registrera dig</Link>
+    </p>
 );
 
 const SignUpForm = withRouter(withFirebase(SignUpFormBase));
