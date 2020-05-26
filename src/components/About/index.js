f4f7fd5eda6d852e5b2dbe576af2ddd6e6374bca
Carl Lindgren <carlindgren@outlook.com>
2020-05-26 16:06:16 +0200

    responsive about-page

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -5,84 +5,82 @@ import styled, { css } from 'styled-components';
 
 const PitchWrap = styled.div`
     position: relative;
-    min-width: 576px;
+    width: 576px;
+    min-width: 400px;
     height: 20%;
-
-    @media all and (max-width: 480px) {
+    @media all and (max-width: 913px) {
+        display: none;
+    }
+    /*     @media all and (max-width: 480px) {
         width: 100vw;
         height: 90vw;
-    }
-
-    ${p =>
-        p.stageName === 'bench' &&
-        css`
-            opacity: 0.5;
-            filter: /* grayscale(1) */ brightness(70%);
-        `}
+    } */
 `;
 
 const PitchImg = styled.img`
     width: 100%;
     max-width: 576px;
-
-    ${p =>
-        p.stageName === 'bench' &&
-        css`
-            opacity: 0.5;
-        `};
+    min-width: 400px;
 `;
 
 const InfoText = styled.div`
     background-color: rgba(0, 0, 0, 0.5);
 `;
 
+const How = styled.div`
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: flex-end;
+    height: 44%;
+    width: 70%;
+
+    @media all and (max-width: 913px) {
+        justify-content: center;
+        align-items: center;
+        height: 70%;
+    }
+`;
+
+const Ptag = styled.p`
+    width: 380px;
+    font-size: 1.1em;
+    line-height: 1.5em;
+    color: white;
+
+    @media all and (max-width: 913px) {
+        font-size: 1.5em;
+        line-height: 1.7em;
+    }
+`;
+const AboutPage = styled.div`
+    overflow: hidden;
+    display: flex;
+    background-color: rgba(0, 0, 0, 0.5);
+    height: 100vh;
+    justify-content: center;
+`;
 const About = ({ location }) => (
-    <div
-        className="AboutPage"
-        style={{
-            display: 'flex',
-            backgroundColor: 'rgba(0, 0, 0, 0.5)',
-            height: '100vh',
-            justifyContent: 'center',
-            overflow: 'hidden'
-        }}
-    >
+    <AboutPage className="AboutPage">
         {/* <Navigation pathname={location.pathname} /> */}
-        <div
-            className="How"
-            style={{
-                display: 'flex',
-                flexDirection: 'row',
-                justifyContent: 'space-between',
-                alignItems: 'flex-end',
-                height: '44%',
-                width: '70%'
-            }}
-        >
+        <How className="How">
             <PitchWrap>
                 <PitchImg src={pitchImg} className="PitchImg" />
             </PitchWrap>
 
             <InfoText style={{ height: '20%' }}>
                 <h1 style={{ fontSize: '2.1em', color: 'white' }}>Hur fungerar det?</h1>
-                <p
-                    style={{
-                        width: '380px',
-                        fontSize: '1.1em',
-                        lineHeight: '1.5em',
-                        color: 'white'
-                    }}
-                >
+                <Ptag>
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
                     incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis
                     nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
                     Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
                     fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
                     culpa qui officia deserunt mollit anim id est laborum.
-                </p>
+                </Ptag>
             </InfoText>
-        </div>
-    </div>
+        </How>
+    </AboutPage>
 );
 
 export default About;
